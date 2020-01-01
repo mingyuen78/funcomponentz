38d79310554d109f92762c053ed1af8232076525
Alex Tam <alex.tam@live.com>
2020-01-01 14:48:06 +0800

    recheckin docs

---
+++ docs/precache-manifest.b4f7e99886a28a7ffb7738e943474ee6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72338f2d145cb48f32a36a7e405a002e",
+    "url": "/funcomponentz/index.html"
+  },
+  {
+    "revision": "678c4eb86146da50418b",
+    "url": "/funcomponentz/static/css/2.724f8f04.chunk.css"
+  },
+  {
+    "revision": "989208f038a3431615f7",
+    "url": "/funcomponentz/static/css/main.ef5b416a.chunk.css"
+  },
+  {
+    "revision": "678c4eb86146da50418b",
+    "url": "/funcomponentz/static/js/2.d1034eb5.chunk.js"
+  },
+  {
+    "revision": "043d189b13d7f3110f83521e56ce6a1e",
+    "url": "/funcomponentz/static/js/2.d1034eb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "989208f038a3431615f7",
+    "url": "/funcomponentz/static/js/main.fab66fc0.chunk.js"
+  },
+  {
+    "revision": "22e3cd519e4f95a6b681",
+    "url": "/funcomponentz/static/js/runtime-main.6f3f2cc5.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/funcomponentz/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/funcomponentz/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/funcomponentz/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  }
+]);
